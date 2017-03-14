7cd3323494ebadbe38785d09bdd41abd2ecd4c47
fasminelee <fasminelee@gmail.com>
2017-03-14 17:38:01 +0800

    Bugfixes

--- 002-LinearLayout/app/src/main/java/com/fasminelee/a002_linearlayout/MainActivity.java
+++ 002-LinearLayout/app/src/main/java/com/fasminelee/a002_linearlayout/MainActivity.java
@@ -1,13 +1,52 @@
 package com.fasminelee.a002_linearlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button mLinearLayoutBtn;
+    public Button mRelativeLayoutBtn;
+    public Button mTableLayoutBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mLinearLayoutBtn = (Button)findViewById(R.id.linearlayout_button);
+        mLinearLayoutBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mRelativeLayoutBtn = (Button)findViewById(R.id.relativelayout_button);
+        mRelativeLayoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mTableLayoutBtn = (Button)findViewById(R.id.tablelayout_button);
+        mTableLayoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TableLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
